1db81d040f4241268c61035020c83396bc5ababf
jonathanroberts2000 <47385932+jonathanroberts2000@users.noreply.github.com>
2019-02-16 22:45:29 -0300

    Add files via upload

--- jsInstruccionIF (9).js
+++ jsInstruccionIF (9).js
@@ -1,6 +1,9 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	var num = Math.floor(Math.random() * 10 + 1);
+	var num;
+	//num = Math.floor(Math.random() * ((10 + 1) - 1) + 1);
+	num = Math.round(Math.random() * (10 - 1) + 1);
 	alert(num);
+
 }//FIN DE LA FUNCIÓN
