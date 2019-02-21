27bf8dc2d677803741cd0705a3e95a8569335291
jonathanroberts2000 <47385932+jonathanroberts2000@users.noreply.github.com>
2019-02-21 12:28:33 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -3,11 +3,13 @@ function Mostrar()
 
 	var contador=0;
 	var acumulador=0;
-	while(contador == 0){
-		acumulador = parseInt(prompt("Ingrese un numero"));
-		
+	var promedio;
+	while(contador < 5){
+		numero = parseInt(prompt("Ingrese un numero:"));
+		acumulador = acumulador + numero;
+		contador = contador + 1;
 	}
-
+	promedio = acumulador / 5;
 
 	document.getElementById("suma").value = acumulador;
 	document.getElementById("promedio").value = acumulador/5;
