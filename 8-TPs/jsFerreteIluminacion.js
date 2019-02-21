9dfad02221c94621654d085920723e91f55c920d
jonathanroberts2000 <47385932+jonathanroberts2000@users.noreply.github.com>
2019-02-21 10:44:39 -0300

    Add files via upload

--- 8-TPs/jsFerreteIluminacion.js
+++ 8-TPs/jsFerreteIluminacion.js
@@ -20,7 +20,56 @@ function CalcularPrecio ()
     var interesIIBB;
     cantidad = parseInt(document.getElementById("Cantidad").value);
     marca = document.getElementById("Marca").value; 
-    if (cantidad >= 6){
+    
+    switch(cantidad){
+        case 1:
+        case 2:
+        {
+            descuento = 0;
+            break;
+        }
+        case 3:
+        {
+             if (marca == "ArgentinaLuz"){
+            descuento = precio * 15/100;
+        }
+        else if (marca == "FelipeLamparas"){
+            descuento = precio * 10/100;
+        }
+        else{
+            descuento = precio * 5/100;
+            break;
+        } 
+        }
+        case 4:
+        {
+            if (marca == "ArgentinaLuz" || marca == "FelipeLamparas"){
+            descuento = precio * 25/100;
+        }
+        else{
+            descuento = precio * 20/100;
+            break;
+        }
+        }
+        case 5:
+        {
+             if(marca == "ArgentinaLuz"){
+            descuento = precio * 40/100;
+        }
+        else{
+            descuento = precio * 30/100;
+            break;
+        }
+        }
+        default:
+        {
+            descuento = precio * 50/100;
+            break;
+        }
+    }
+    
+    
+   /* if (cantidad >= 6){
         descuento = precio * 50/100;
         }
     else if (cantidad == 5){
@@ -53,7 +102,7 @@ function CalcularPrecio ()
     else{
         descuento = 0;
         precioFinal = cantidad * precio;
-    }
+    }*/
         precioDescuento = precio - descuento;
         document.getElementById("precioDescuento").value = precioDescuento;
         precioFinal = precioDescuento * cantidad;
@@ -66,3 +115,4 @@ function CalcularPrecio ()
         alert("El precio final a abonar es de" + " " + "$" + precioFinal);
         }
 }
+
