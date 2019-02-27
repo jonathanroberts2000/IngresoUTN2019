cabb2a1d076173ad2c966c30835e959acbda334c
jonathanroberts2000 <47385932+jonathanroberts2000@users.noreply.github.com>
2019-02-27 19:19:36 -0300

    Add files via upload

--- 6-InstruccionFor/jsIteracionesFor (5).js
+++ 6-InstruccionFor/jsIteracionesFor (5).js
@@ -1,7 +1,10 @@
 function Mostrar()
 {
-
-
-
-
+    var numero = parseInt(prompt("Ingrese un numero"));
+    for(var i = 0; i != 9; numero = parseInt(prompt("Ingrese un numero"))){
+        if(numero == 9){
+            console.log(numero);
+            break;
+        }
+    }
 }//FIN DE LA FUNCIÓN
--- 6-InstruccionFor/jsIteracionesFor (6).js
+++ 6-InstruccionFor/jsIteracionesFor (6).js
@@ -1,6 +1,14 @@
 function Mostrar()
 {
-
+    var numero = parseInt(prompt("Ingrese un numero"));
+    var numerosPares;
+    for(var i = 0; i = confirm("Desea ingresar mas numeros?") == true; numero = parseInt(prompt("Ingrese un numero"))){
+        numerosPares = numero % 2;
+        if(numerosPares == 0){
+            console.log(numero);
+        }
+    }
+    
 
 
 
