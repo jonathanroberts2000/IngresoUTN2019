8b3f8a4d5d0fec3ee22dc00e6647a927aef32138
jonathanroberts2000 <47385932+jonathanroberts2000@users.noreply.github.com>
2019-02-27 12:26:31 -0300

    Add files via upload

--- 6-InstruccionFor/jsIteracionesFor (3).js
+++ 6-InstruccionFor/jsIteracionesFor (3).js
@@ -1,8 +1,8 @@
 function Mostrar()
 {
-
-var repetciones = prompt("ingrese el número de repeticiones");
-
-
+    var repeticiones = parseInt(prompt("ingrese el número de repeticiones"));
+    for(var i = 0; i < repeticiones; i++){
+        console.log("Hola UTN FRA");
+    } 
 
 }//FIN DE LA FUNCIÓN
