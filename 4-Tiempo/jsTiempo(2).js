602692343c07c99d7981188187c3cdf7465a1ac8
jonathanroberts2000 <jonathanroberts992@gmail.com>
2019-04-07 19:25:56 -0300

    Add files via upload

--- 4-Tiempo/jsTiempo(2).js
+++ 4-Tiempo/jsTiempo(2).js
@@ -1,22 +1,15 @@
-var miIntervalo ;
+var miIntervalo;
 
-
-function inicio()
-{
+function inicio(){
 	alert("function inicio.");
 	miIntervalo = setInterval(SegundosEnElAire, 3000);
-	
 }//FIN DE LA FUNCIÓN inicio
 
-function SegundosEnElAire() {
-
-    alert("Bienvenido a la UTN FRA");
-    
+function SegundosEnElAire(){
+    alert("Bienvenido a la UTN FRA"); 
 }//FIN DE LA FUNCIÓN SegundosEnElAire
 
-function fin()
-{
+function fin(){
 	alert("function fin.");
 	clearTimeout(miIntervalo);
-
 }//FIN DE LA FUNCIÓN fin
